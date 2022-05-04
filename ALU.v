e796d617b1bdbe8665e66b68401a981d9f4688a5
thedrippster <89713356+thedrippster@users.noreply.github.com>
2022-05-04 00:05:47 -0400

    Update ALU.v

--- ALU.v
+++ ALU.v
@@ -6,19 +6,20 @@ module ALU(a, b, sel, out, stat);
 	output reg [31:0] out;
 	output [3:0] stat;
 	
-	wire cout;
+	reg cout;
+	wire cout1, cout2;
 	wire [31:0] sum;
 	wire [31:0] dif;
 	
-	adder m0 (a, b, sum, cout);
-	adder m3 ((a+1), (~b), dif, cout);
+	adder m0 (a, b, sum, cout1);
+	adder m3 ((a+1), (~b), dif, cout2);
 	
 	always @ (*) begin
 		case (sel)
-			3'b000: out <= sum; //ADD, load, store
+			3'b000: begin out <= sum; cout <= cout1; end //ADD, load, store
 			3'b001: out <= a | b; //OR
 			3'b010: out <= a << b[4:0]; //SLL
-			3'b011: out <= dif; //beq
+			3'b011: begin out <= dif; cout <= cout1; end //beq
 			default: out = 32'bx;
 		endcase
 	end
@@ -29,6 +30,3 @@ module ALU(a, b, sel, out, stat);
 	assign stat[0] = cout; //c - carryout flag
 	
 endmodule
-
-
-	
